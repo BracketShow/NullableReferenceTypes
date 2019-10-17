61cac0e0356e9288fdb3370cd304f46ec1bc9cfd
Eric De Carufel <edecarufel@pyxis-studio.com>
2019-10-16 21:46:19 -0400

    Add project files.

---
+++ NullableReferenceTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NullableReferenceTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person("Eric", "De Carufel");
+            var length = GetMiddleNameLength(person);
+            Console.WriteLine(length);
+        }
+
+        private static int GetMiddleNameLength(Person person)
+        {
+            var middle = person.MiddleName;
+            return middle.Length;
+        }
+    }
+}
